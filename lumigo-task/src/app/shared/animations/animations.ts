df01d2784d12333b2c5efad8db929e594cefa3e0
Gad Dahan <gadd@loona-app.com>
2022-07-27 11:35:24 +0300

    fix menu hidden

--- lumigo-task/src/app/shared/animations/animations.ts
+++ lumigo-task/src/app/shared/animations/animations.ts
@@ -1,7 +1,7 @@
 import { animate, state, style, transition, trigger } from "@angular/animations";
 
 export const showHideList = trigger('showHideList', [	
-	state('false', style({  minHeight: '2rem', overflowY: 'auto' })),
+	state('false', style({  minHeight: '2rem', overflowY: 'initial',  })),
 	state('true', style({ height: 0, overflowY: 'hidden', minHeight: 0 })),
 	transition('* <=> *', animate('400ms ease-out'))
 ]);
